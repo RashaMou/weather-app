bab24a1693e405a6fe043578230e03ba12f3ce03
Rasha Moumneh <rasha@fastmail.com>
2020-01-10 15:58:07 -0500

    Create background map

--- src/App.js
+++ src/App.js
@@ -4,9 +4,15 @@ import Map from "./components/Map";
 import WeatherCard from "./components/WeatherCard";
 
 function App() {
+  let key = "AIzaSyBcv2QVgQ6oRSdKqZDOKxAGAicxoEBZTBo";
   return (
-    <div className="App">
-      <Map />
+    <div className="App" style={{ width: "100vw", height: "100vh" }}>
+      <Map
+        googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=${process.env.REACT_APP_GOOGLE_MAPS_KEY}`}
+        loadingElement={<div style={{ height: "100%" }} />}
+        containerElement={<div style={{ height: "100%" }} />}
+        mapElement={<div style={{ height: "100%" }} />}
+      />
       <WeatherCard />
     </div>
   );
--- src/components/Map.js
+++ src/components/Map.js
@@ -1,11 +1,19 @@
 import React from "react";
+import { GoogleMap, withScriptjs, withGoogleMap } from "react-google-maps";
 
-const Map = () => {
+const BackgroundMap = () => {
   return (
-    <div>
-      <h2>Map</h2>
-    </div>
+    <GoogleMap
+      defaultZoom={10}
+      defaultCenter={{ lat: 39.952583, lng: -75.165222 }}
+      defaultOptions={{
+        disableDefaultUI: true,
+        mapTypeId: "satellite"
+      }}
+    ></GoogleMap>
   );
 };
+
+const Map = withScriptjs(withGoogleMap(BackgroundMap));
 
 export default Map;
