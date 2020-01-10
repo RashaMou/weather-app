88f9187f3a35349cb39b302d516290d176ed5213
Rasha Moumneh <rasha@fastmail.com>
2020-01-09 20:33:32 -0500

    App scaffolding

---
+++ src/components/WeatherCard.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Search from "./Search";
+import WeatherInfo from "./WeatherInfo";
+
+const WeatherCard = () => {
+  return (
+    <div>
+      <Search />
+      <WeatherInfo />
+    </div>
+  );
+};
+
+export default WeatherCard;
---
+++ src/components/WeatherInfo.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const WeatherInfo = () => {
+  return (
+    <div>
+      <h2>WeatherInfo</h2>
+    </div>
+  );
+};
+
+export default WeatherInfo;
