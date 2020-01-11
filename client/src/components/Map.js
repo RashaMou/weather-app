2775d2b2dec6377d56ee1c56fabd5fbb9622466b
Rasha Moumneh <rasha@fastmail.com>
2020-01-11 18:14:12 -0500

    Make reverse geocode axios call in Map.js component

--- client/src/components/Map.js
+++ client/src/components/Map.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useContext } from "react";
+import React, { useEffect, useContext } from "react";
 import axios from "axios";
 import { GoogleMap, withScriptjs, withGoogleMap } from "react-google-maps";
 import LocationContext from "../contexts/LocationContext";
@@ -7,11 +7,6 @@ const BackgroundMap = () => {
   const { setCoordinates, setForecast, coordinates } = useContext(
     LocationContext
   );
-
-  const [currentLoc, getCurrentLoc] = useState({
-    lat: "",
-    lng: ""
-  });
 
   let lat = coordinates.lat;
   let lng = coordinates.lng;
@@ -23,6 +18,12 @@ const BackgroundMap = () => {
         lng: position.coords.longitude
       };
       setCoordinates(pos);
+      axios
+        .post("http://localhost:5000/api/reversegeocode", pos)
+        .then(res => {
+          console.log(res);
+        })
+        .catch(err => console.log(err));
     });
   };
 
