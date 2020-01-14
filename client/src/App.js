32cf22d248bd674efc9418e70437d6ade99bbeb4
Rasha Moumneh <rasha@fastmail.com>
2020-01-14 18:10:31 -0500

    Working branch

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,55 @@
-import React from "react";
+import React, { useEffect, useContext } from "react";
 import Map from "./components/Map";
+import axios from "axios";
+
 import WeatherCard from "./components/WeatherCard";
-import GlobalState from "./contexts/GlobalState";
+import LocationContext from "./contexts/LocationContext";
+import getInitialUserLocation from "./helpers/getInitialUserLocation";
 
 function App() {
   const apiKey = "AIzaSyBcv2QVgQ6oRSdKqZDOKxAGAicxoEBZTBo";
+  const {
+    setCoordinates,
+    coordinates,
+    lat,
+    lng,
+    setForecast,
+    city
+  } = useContext(LocationContext);
+
+  useEffect(() => {
+    getInitialUserLocation(setCoordinates);
+    axios
+      .post("http://localhost:5000/api/weather", coordinates)
+      .then(res => {
+        console.log("res.data weather", res.data);
+        setForecast(res.data.currently);
+        setCoordinates({ lat: res.data.latitude, lng: res.data.longitude });
+      })
+      .catch(err => console.log(err));
+
+    axios
+      .post("http://localhost:5000/api/reversegeocode", { lat, lng })
+      .then(res => {
+        console.log("res.data", res.data);
+        // setCity(res.data[0].formatted_address);
+      })
+      .catch(err => console.log(err));
+
+    console.log("city from map useeffect", city);
+  }, []);
 
   return (
-    <GlobalState>
-      <div className="background">
-        <Map
-          className="map"
-          googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=${apiKey}`}
-          loadingElement={<div style={{ height: "100%" }} />}
-          containerElement={<div style={{ height: "100%" }} />}
-          mapElement={<div style={{ height: "100%" }} />}
-        />
-        <WeatherCard />
-      </div>
-    </GlobalState>
+    <div className="background">
+      <Map
+        className="map"
+        googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=${apiKey}`}
+        loadingElement={<div style={{ height: "100%" }} />}
+        containerElement={<div style={{ height: "100%" }} />}
+        mapElement={<div style={{ height: "100%" }} />}
+      />
+      <WeatherCard />
+    </div>
   );
 }
 
--- client/src/components/Map.js
+++ client/src/components/Map.js
@@ -16,36 +16,36 @@ const BackgroundMap = () => {
   let lat = coordinates.lat;
   let lng = coordinates.lng;
 
-  useEffect(() => {
-    getInitialUserLocation(setCoordinates);
+  // useEffect(() => {
+  //   getInitialUserLocation(setCoordinates);
+  //   axios
+  //     .post("http://localhost:5000/api/weather", { lat, lng })
+  //     .then(res => {
+  //       console.log("res.data weather", res.data);
+  //       setForecast(res.data.currently);
+  //       setCoordinates({ lat: res.data.latitude, lng: res.data.longitude });
+  //     })
+  //     .catch(err => console.log(err));
 
-    axios
-      .post("http://localhost:5000/api/weather", { lat, lng })
-      .then(res => {
-        console.log("weather", res.data.currently);
-        setForecast(res.data.currently);
-      })
-      .catch(err => console.log(err));
+  //   axios
+  //     .post("http://localhost:5000/api/reversegeocode", { lat, lng })
+  //     .then(res => {
+  //       console.log("res.data", res.data);
+  //       // setCity(res.data[0].formatted_address);
+  //     })
+  //     .catch(err => console.log(err));
 
-    axios
-      .post("http://localhost:5000/api/reversegeocode", { lat, lng })
-      .then(res => {
-        setCity(res.data[0].formatted_address);
-        console.log(res.data[0].formatted_address);
-      })
-      .catch(err => console.log(err));
-
-    console.log("city from map useeffect", city);
-  }, []);
+  //   console.log("city from map useeffect", city);
+  // }, []);
 
   return (
     <GoogleMap
       defaultZoom={10}
-      center={{ lat, lng }}
       defaultOptions={{
         disableDefaultUI: true,
         mapTypeId: "satellite"
       }}
+      center={{ lat, lng }}
     ></GoogleMap>
   );
 };
